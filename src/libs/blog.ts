840255be4395f61868796c684555d6e59f3ba84c
KeitaUenishi <keita.uenishi@gmail.com>
2026-01-14 23:23:36 +0900

    init commit

---
+++ src/libs/blog.ts
@@ -0,0 +1,25 @@
+// src/lib/note.ts
+import { getCollection } from 'astro:content';
+import type { CollectionEntry } from 'astro:content';
+
+export async function getAllPublicBlogs(): Promise<CollectionEntry<'blog'>[]> {
+  const notes = await getCollection('blog', ({ data }) => {
+    return import.meta.env.DEV || data.public === true;
+  });
+  return notes.sort((a, b) => 
+    b.data.publishedAt.getTime() - a.data.publishedAt.getTime()
+  );
+}
+
+// ユーティリティ関数
+export function formatDate(date: Date): string {
+  console.log(date);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}.${month}.${day}`;
+}
+
+export function generateBlogPath(id: string): string {
+  return `/blog/${id}`;
+}
